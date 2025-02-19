900dac6f7051092e48e4eab9e89a01d3c662b554
a17673370400 <17673370400@163.com>
2025-02-19 20:43:48 +0800

    Update MyToken.sol

--- deploy/MyToken.sol
+++ deploy/MyToken.sol
@@ -10,3 +10,7 @@ contract MyToken is ERC20 {
         _mint(msg.sender, 1e10*1e18);
     } 
 }
+
+Deployer: 0xA2994BaBAd41563100357Ff8caB1442EFED3678C
+Deployed to: 0xFDfA91105B25DCf747d949D4ee4C87dfc87ED5e9
+Transaction hash: 0xb876f62a40457ec5e174a7107c70c4e134be21e197ab9520b46b3b08bbaafbfd
